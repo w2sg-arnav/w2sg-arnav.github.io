82dfff508c504a83d31a5727a08b696c7f175003
arnavmcmxvi <sonavane.arnav2@gmail.com>
2024-11-01 08:22:57 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/w2sg-arnav.github.io',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
